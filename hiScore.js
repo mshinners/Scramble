c51a9b8c2bbc29824ee13bf06a70f0ec61bc7510
Michael Shinners <mishinners@icloud.com>
2017-08-28 11:48:33 -0700

    building table

--- hiScore.js
+++ hiScore.js
@@ -9,25 +9,21 @@ function NewScore(userName, score) {
   highScoreList.push(this);
 }
 
-var test1 = new NewScore('bob', 23);
-var test2 = new NewScore('Al', 2);
-var test3 = new NewScore('Jane', 45);
-var test4 = new NewScore('Sara', 5);
-var test5 = new NewScore('Vince', 15);
-
 highScoreList.sort(function(a,b) {
   return a.score - b.score;
 });
-
-
+var table = document.getElementById('hiScoreTable');
+var tr = document.createElement('tr');
+var name = document.createElement('td');
+name.innerText = name;
+var score = document.createElement('td');
+hiScoreTable.appendChild(tr);
 //
-// function compare(a,b) {
-//   if (a.score)
-// }
-
-// var highScoreList = [];
-
-//
+// var test1 = new NewScore('bob', 23);
+// var test2 = new NewScore('Al', 2);
+// var test3 = new NewScore('Jane', 45);
+// var test4 = new NewScore('Sara', 5);
+// var test5 = new NewScore('Vince', 15);
 // [i].score
 
 // X create an object that is the score & userName
