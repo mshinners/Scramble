64a8ae405f79a4bad78886248473dfefdea18718
Michael Shinners <mishinners@icloud.com>
2017-08-29 14:00:29 -0700

    aboutUs.js work

--- aboutUs.js
+++ aboutUs.js
@@ -1,15 +1,15 @@
 'use strict';
 var target;
 //each create an object with our name, favorite word, & blurb about us?
-var michelle = {name:'Michelle', faveWord: 'word', blurb: 'This is what Michelle says'};
-var michael = {name:'Michael', faveWord: 'word', blurb: 'This is what Michael says'};
-var nathan = {name:'Nathan', faveWord: 'word', blurb: 'This is what Nathan says'};
-var shannon = {name:'Shannon', faveWord: 'word', blurb: 'This is what Shannon says'};
+var michelle = {dev:'Michelle', faveWord: 'word', blurb: 'This is what Michelle says'};
+var michael = {dev:'Michael', faveWord: 'word', blurb: 'This is what Michael says'};
+var nathan = {dev:'Nathan', faveWord: 'word', blurb: 'This is what Nathan says'};
+var shannon = {dev:'Shannon', faveWord: 'word', blurb: 'This is what Shannon says'};
 //Store objects in an array
 var people = [michelle, michael, nathan, shannon];
 
 //get all of the elements with a class name of profile & add an eventListener
-var learnMore = document.getElementsByClassName('learnMore');
+var learnMore = document.getElementsByClassName('profile');
 for (var i = 0; i < people.length; i++){
   learnMore[i].addEventListener('click', createProfile);
   learnMore[i].addEventListener('mouseover', changeText);
@@ -22,7 +22,7 @@ function createProfile(event) {
   event.target.removeEventListener('click', createProfile);
   //get the correct Person object
   for (var i = 0; i < people.length; i++){
-    if (event.target.id === people[i].name.toLowerCase()){
+    if (event.target.id === people[i].dev.toLowerCase()){
       target = people[i];
     }
   }
@@ -37,9 +37,9 @@ function createProfile(event) {
   profile.appendChild(paragraph);
 }
 
-function changeText() {
-  event.target.innerText = 'Learn more';
-}
+// function changeText() {
+//   event.target.innerText = 'Learn more';
+// }
 
 function originalText() {
   event.target.innerText = event.target.id;
