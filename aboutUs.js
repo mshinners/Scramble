c6e4e1c0583ac491db28e18fbcfa2dc52669a3b3
Michael Shinners <mishinners@icloud.com>
2017-08-30 10:35:32 -0700

    aboutMe CSSing

--- aboutUs.js
+++ aboutUs.js
@@ -1,10 +1,10 @@
 'use strict';
 var target;
 //each create an object with our name, favorite word, & blurb about us?
-var michelle = {dev:'Michelle', faveWord: 'Three', blurb: 'Coding is fun!'};
-var michael = {dev:'Michael', faveWord: 'Acrid', blurb: 'Dad Jokes Rule!'};
-var nathan = {dev:'Nathan', faveWord: 'Szygy', blurb: 'Hi Momma!'};
-var shannon = {dev:'Shannon', faveWord: 'Their', blurb: 'Thanks for playing!'};
+var michelle = {dev:'Michelle', faveWord: 'Merge', blurb: 'Coding is fun!'};
+var michael = {dev:'Michael', faveWord: 'Pithy', blurb: 'Dad Jokes Rule!'};
+var nathan = {dev:'Nathan', faveWord: 'Djinn', blurb: 'Hi Momma!'};
+var shannon = {dev:'Shannon', faveWord: 'Amuck', blurb: 'Thanks for playing!'};
 //Store objects in an array
 var people = [michelle, michael, nathan, shannon];
 
@@ -16,7 +16,6 @@ for (var i = 0; i < people.length; i++){
 }
 function createProfile(event) {
   var devProfile = document.getElementById(event.target.id);
-  console.log(devProfile);
   //get the correct Person object
   for (var i = 0; i < people.length; i++){
     if (event.target.id === people[i].dev.toLowerCase()){
@@ -26,7 +25,7 @@ function createProfile(event) {
   var paragraph = document.getElementById('learnMoreWindow');
   // paragraph.setAttribute('class', 'displayInline');
   var faveWord = document.createElement('p');
-  faveWord.innerText = target.faveWord;
+  faveWord.innerText = 'My Favorite Five-letter Word: ' + target.faveWord;
   faveWord.setAttribute('id', 'faveWord');
   paragraph.appendChild(faveWord);
   var blurb = document.createElement('p');
@@ -35,11 +34,9 @@ function createProfile(event) {
   paragraph.appendChild(blurb);
 };
 function removeProfile(event) {
-  // var cutProfile = document.getElementById(event.target.id);
   var cutProfile = document.getElementById('learnMoreWindow');
   var faveWord = document.getElementById('faveWord');
   var blurb = document.getElementById('blurb');
-  console.log(faveWord);
   cutProfile.removeChild(faveWord);
   cutProfile.removeChild(blurb);
 };
